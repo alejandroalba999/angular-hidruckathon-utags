ce139ca06b6c55e3ecc7461bb8322e97787ac356
edgar <edgar.alba>
2021-09-22 15:21:40 -0500

    enviroments heroku nodejs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  urlGlobal: 'http://172.16.50.41:3005/api',
+  urlGlobal: 'https://heroku-hidro-node.herokuapp.com/api',
   production: true,
-  muestraImagen: 'http://172.16.50.41:3005/api/imagenes/obtener/',
+  muestraImagen: 'https://heroku-hidro-node.herokuapp.com/api/obtener/',
 };
