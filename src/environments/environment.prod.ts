a42337d7e30f3a823764392668c1e4487050c3f0
edgar <edgar.alba>
2021-10-15 12:25:00 -0500

    s

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  urlGlobal: 'http://localhost:3000/api/', /*https://heroku-hidro-node.herokuapp.com/api*/
+  urlGlobal: 'https://heroku-hidro-node.herokuapp.com/api/', /*https://heroku-hidro-node.herokuapp.com/api*/
   production: true,
-  muestraImagen: 'http://localhost:3000/api/imagenes/obtener/',
+  muestraImagen: 'https://heroku-hidro-node.herokuapp.com/api/imagenes/obtener/',
 };
